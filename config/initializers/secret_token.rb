16dfdf2495f06686c8ab9ad8b823f8298ed1edc3
Devon Moss <dvnmss@gmail.com>
2010-12-17 21:16:12 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recipro::Application.config.secret_token = '797ac37a4922ee6b4cbe184d47bf2428c2c5d292041f1ca74d4cb1e672cafaedcad7456bb1f38ad73250cc866eb4c50e7c809dfc7fb5145708f0db1331ce6220'
